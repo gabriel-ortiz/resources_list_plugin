10aa9418fb7aff0e31d4a04464c48554ccbea205
Gabriel Ortiz <gabriel_ortiz@cuc.claremont.edu>
2017-06-21 15:57:40 +0000

    configured wp api to display custom meta boxes

--- resource-lists.php
+++ resource-lists.php
@@ -29,6 +29,9 @@ class resource_list{
         
         //enqueue admin scripts
         add_action( 'admin_enqueue_scripts', [$this, 'add_admin_scripts' ] );
+        
+        //adding custom fields to rest API
+        add_action('rest_api_init', [$this,'register_custom_fields']);
 
     }
     
@@ -70,9 +73,52 @@ class resource_list{
         
     }//end of add_admin_scripts
     
+    public function register_custom_fields(){
+        register_rest_field(
+              $object_type  = 'resource_list_item',
+              $attribute    = 'record_title',
+              $args         = array(
+                                    'get_callback' => [$this, 'show_fields']
+                                )
+            );
+            
+            
+        register_rest_field(
+              $object_type  = 'resource_list_item',
+              $attribute    = 'record_author',
+              $args         = array(
+                                    'get_callback' => [$this, 'show_fields']
+                                )
+            );
+            
+        register_rest_field(
+              $object_type  = 'resource_list_item',
+              $attribute    = 'record_cover',
+              $args         = array(
+                                    'get_callback' => [$this, 'show_fields']
+                                )
+            );
+            
+        register_rest_field(
+              $object_type  = 'resource_list_item',
+              $attribute    = 'record_URL',
+              $args         = array(
+                                    'get_callback' => [$this, 'show_fields']
+                                )
+            );  
+            
+        register_rest_field(
+              $object_type  = 'resource_list_item',
+              $attribute    = 'record_isbn',
+              $args         = array(
+                                    'get_callback' => [$this, 'show_fields']
+                                )
+            );              
+    }
     
-    
-    
+    public function show_fields($object, $field_name, $request){
+        return get_post_meta($object['id'], $field_name, true );
+    }
 }
 
 $init_resource_plugin = new resource_list();
