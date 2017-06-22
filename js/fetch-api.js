c606bbf4e0fa0dee6330e219c90e4f1835f27b7a
Gabriel Ortiz <gabriel_ortiz@cuc.claremont.edu>
2017-06-22 02:25:56 +0000

    fixed some scoping with JS

--- js/fetch-api.js
+++ js/fetch-api.js
@@ -1,149 +1,162 @@
-jQuery(document).ready(function($) {
 
-/*
-    CACHED ELEMENTS FROM DOM
-*/
-    var $this = this;
+( function( window, $ ) {
+	'use strict';
+	var document = window.document;
 
-    //grab the DOM elements
-    $this.search_api = $('#search_api');
-
-    $this.record_title = $('#record_title');
-    $this.record_author = $('#record_author');
-    $this.record_cover = $('#record_cover');
-    $this.catalog_url = $('#record_URL');
-
-    $this.isbn = $("[name='record_isbn']");
-
-
-/*
-        VARIABLES AND HTML OBJECTS 
-*/
-    //set up google API variables
-    $this.google_base = 'https://www.googleapis.com/books/v1/volumes?q=isbn%3A';
-    $this.google_key = '&key=' + RESOURCE_LISTS_SETTINGS.google_api;
+    var api_data = {};
     
-    //build up img element
-    $this.preview_cover = $('<img />');
-        $this.preview_cover.attr('src', $this.record_cover.val());
-        $this.preview_cover.attr('id', 'preview_image');
-
-    //check to see if image exists
-    if ($this.record_cover.val().length !== 0) {
-        console.log('image detected', $this.record_cover.val());
-
-        $this.preview_cover
-            .hide()
-            .insertAfter($this.record_cover)
-            .fadeIn('1200');
-    }
-
-    $this.loading_image = $('<img />');
-        $this.loading_image.attr('id', 'loading-animation');
-        $this.loading_image.attr('src', RESOURCE_LISTS_SETTINGS.loading);
-        $this.loading_image.attr('alt', 'loading');    
-
-
-/*
-    FUNCTIONS
-*/
-    $this.fetch_api_data = function() {
-        //show loading image
-        $this.loading_image.show();
-
-        //grad the value of isbn
-        $this.isbnValue = $this.isbn.val();
-        console.log($this.isbnValue);
-
-        //setup catalog URL
-        $this.record_path = RESOURCE_LISTS_SETTINGS.catalog_url + $this.isbnValue + RESOURCE_LISTS_SETTINGS.catalog_closing;
-
-        $this.google_api = $this.google_base + $this.isbnValue + $this.google_key;
-
-        $.ajax({
-            url: $this.google_api,
-            type: 'GET',
-            dataType: 'jsonp',
-            success: function(data) {
-                //SUCCESS!
-
-                //hide loading image
-                $this.loading_image.remove();
-
-                if (data.totalItems > 0) {
-
-                    var author_array = data.items[0].volumeInfo.authors;
-                    author_array.join(', ');
-
-                    $this.record_title.val(data.items[0].volumeInfo.title);
-                    $this.record_author.val(author_array);
-                    $this.record_cover.val(data.items[0].volumeInfo.imageLinks.thumbnail);
-
-                    $this.preview_cover
-                        .attr('src', data.items[0].volumeInfo.imageLinks.thumbnail)
-                        .hide()
-                        .insertAfter($this.record_cover)
-                        .fadeIn('1200');
-
-                    //$this.preview_cover.attr('src', data.items[0].volumeInfo.imageLinks.thumbnail );
-                    $this.catalog_url.val($this.record_path);
-
-                }
-                else {
-                    //no results found
-                    $.ajax({
-                        type: 'POST',
-                        url: ajaxurl,
-                        data: {
-                            "action": "test_function"
-                        },
-                        success: function(response) {
-                            var recordError = $('<div />', {
-                                'class': 'error',
-                                'text': 'Sorry no records were found. Bummer!',
-                                'css': {
-                                    'padding': '10px'
+    api_data.init = function(){
+        
+            //cach this with init
+            var init = this;
+            
+            //grab the DOM elements
+            init.search_api = $('#search_api');
+        
+            init.record_title  = $('#record_title');
+            init.record_author = $('#record_author');
+            init.record_cover  = $('#record_cover');
+            init.catalog_url   = $('#record_URL');
+        
+            init.isbn          = $("[name='record_isbn']");
+        
+        
+        /*
+                VARIABLES AND HTML OBJECTS 
+        */
+            //set up google API variables
+            init.google_base = 'https://www.googleapis.com/books/v1/volumes?q=isbn%3A';
+            init.google_key = '&key=' + RESOURCE_LISTS_SETTINGS.google_api;
+            
+            //build up img element
+            init.preview_cover = $('<img />');
+                init.preview_cover.attr('src', init.record_cover.val());
+                init.preview_cover.attr('id', 'preview_image');
+        
+            //check to see if image exists
+            if (init.record_cover.val().length !== 0) {
+                console.log('image detected', init.record_cover.val());
+        
+                init.preview_cover
+                    .hide()
+                    .insertAfter(init.record_cover)
+                    .fadeIn('700');
+            }
+        
+            init.loading_image = $('<img />');
+                init.loading_image.attr('id', 'loading-animation');
+                init.loading_image.attr('src', RESOURCE_LISTS_SETTINGS.loading);
+                init.loading_image.attr('alt', 'loading'); 
+                
+        /*
+            FUNCTIONS
+        */
+            init.fetch_api_data = function() {
+                //show loading image
+                init.loading_image.show();
+        
+                //grad the value of isbn
+                init.isbnValue = init.isbn.val();
+                console.log(init.isbnValue);
+        
+                //setup catalog URL
+                init.record_path = RESOURCE_LISTS_SETTINGS.catalog_url + init.isbnValue + RESOURCE_LISTS_SETTINGS.catalog_closing;
+        
+                init.google_api = init.google_base + init.isbnValue + init.google_key;
+        
+                $.ajax({
+                    url: init.google_api,
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    success: function(data) {
+                        //SUCCESS!
+        
+                        //hide loading image
+                        init.loading_image.remove();
+        
+                        if (data.totalItems > 0) {
+        
+                            var author_array = data.items[0].volumeInfo.authors;
+                            author_array.join(', ');
+        
+                            init.record_title.val(data.items[0].volumeInfo.title);
+                            init.record_author.val(author_array);
+                            init.record_cover.val(data.items[0].volumeInfo.imageLinks.thumbnail);
+        
+                            init.preview_cover
+                                .attr('src', data.items[0].volumeInfo.imageLinks.thumbnail)
+                                .hide()
+                                .insertAfter(init.record_cover)
+                                .fadeIn('1200');
+        
+                            //init.preview_cover.attr('src', data.items[0].volumeInfo.imageLinks.thumbnail );
+                            init.catalog_url.val(init.record_path);
+        
+                        }
+                        else {
+                            //no results found
+                            $.ajax({
+                                type: 'POST',
+                                url: ajaxurl,
+                                data: {
+                                    "action": "test_function"
+                                },
+                                success: function(response) {
+                                    var recordError = $('<div />', {
+                                        'class': 'error',
+                                        'text': 'Sorry no records were found. Bummer!',
+                                        'css': {
+                                            'padding': '10px'
+                                        }
+                                    });
+        
+                                    $('.wp-heading-inline').after(recordError);
+                                    setTimeout(function() {
+                                        recordError.fadeOut(700, function() {
+                                            this.remove();
+                                        });
+                                    }, 3000);
                                 }
                             });
-
-                            $('.wp-heading-inline').after(recordError);
-                            setTimeout(function() {
-                                recordError.fadeOut(700, function() {
-                                    this.remove();
-                                });
-                            }, 3000);
                         }
-                    });
-                }
-
-            },
-            //error with the search
-            fail: function(errorResponse) {
-                alert(errorResponse);
+        
+                    },
+                    //error with the search
+                    fail: function(errorResponse) {
+                        alert(errorResponse);
+                    }
+                });
+            };//end of fetch api function
+    
+        /*
+        EVENTS  
+    */    
+    
+        //disable wordpress's enter to save function
+        init.isbn.on('keyup keypress', function(e) {
+            var keyCode = e.keyCode || e.which;
+            if (keyCode === 13) {
+                //cancel carriage return
+                e.preventDefault();
+                
+                init.fetch_api_data();
+                return false;
+                
+    
             }
         });
-    };//end of fetch api function
-
     
-/*
-    EVENTS  
-*/    
+        init.search_api.on('click', function() {
+            init.fetch_api_data();
+        });   
 
-    //disable wordpress's enter to save function
-    $this.isbn.on('keyup keypress', function(e) {
-        var keyCode = e.keyCode || e.which;
-        if (keyCode === 13) {
-            //cancel carriage return
-            e.preventDefault();
-            
-            $this.fetch_api_data();
-            return false;
-            
+    console.log('this is working, success!!!');
+    };
 
-        }
+
+    //start the function
+    $(document).ready(function(){
+        api_data.init();
     });
 
-    $this.search_api.on('click', function() {
-        $this.fetch_api_data();
-    });
-});
+} )( this, jQuery );
